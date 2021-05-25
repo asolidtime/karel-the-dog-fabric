08bfc98f9949a83060d80de960bcfaf5c3310fd3
Maxwell Leon <maxwellleon55243@gmail.com>
2021-05-25 14:37:20 -0500

    Add Karelium Ingot

--- src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
+++ src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
@@ -1,14 +1,21 @@
 package net.asolidtime.programmingproject;
 
+import net.asolidtime.programmingproject.items.KareliumIngotItem;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class FinalProgrammingProjectMod implements ModInitializer {
+	public static final KareliumIngotItem KAREL_INGOT_ITEM = new KareliumIngotItem(new FabricItemSettings().group(ItemGroup.MISC));
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
+		Registry.register(Registry.ITEM, new Identifier("programmingproject", "karelium_ingot"), KAREL_INGOT_ITEM);
+		System.out.println("Hello from a programming student!");
 	}
 }
---
+++ src/main/java/net/asolidtime/programmingproject/items/KareliumIngotItem.java
@@ -0,0 +1,10 @@
+package net.asolidtime.programmingproject.items;
+
+import net.minecraft.item.Item;
+
+public class KareliumIngotItem extends Item {
+
+    public KareliumIngotItem(Settings settings) {
+        super(settings);
+    }
+}
