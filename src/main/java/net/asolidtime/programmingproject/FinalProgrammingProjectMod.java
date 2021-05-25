2b2410333ec210df8cfb88603b983ed089186933
Maxwell Leon <maxwellleon55243@gmail.com>
2021-05-25 13:36:56 -0500

    Renaming classes and stuff

--- src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
+++ src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
@@ -1,8 +1,8 @@
-package net.fabricmc.example;
+package net.asolidtime.programmingproject;
 
 import net.fabricmc.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class FinalProjectMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
