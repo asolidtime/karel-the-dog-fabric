edd43abff980c3f6aac02322c98c88133947b7f9
Maxwell Leon <maxwellleon55243@gmail.com>
2021-05-25 15:10:09 -0500

    a bit more rebranding

--- src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
+++ src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
@@ -3,7 +3,6 @@ package net.asolidtime.programmingproject;
 import net.asolidtime.programmingproject.items.KareliumIngotItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
