691af56608fb1e52e525234ecc0c7c30fea59357
Maxwell Leon <maxwellleon55243@gmail.com>
2021-05-29 20:46:51 -0500

    Spawning Karelium Ore and making it drop stuff

--- src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
+++ src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
@@ -1,19 +1,40 @@
 package net.asolidtime.programmingproject;
 
 import net.asolidtime.programmingproject.items.KareliumIngotItem;
+import net.asolidtime.programmingproject.items.KareliumOreBlock;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 public class FinalProgrammingProjectMod implements ModInitializer {
 	public static final KareliumIngotItem KAREL_INGOT_ITEM = new KareliumIngotItem(new FabricItemSettings().group(ItemGroup.MISC));
-	public static final Block KAREL_ORE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+	public static final KareliumOreBlock KAREL_ORE_BLOCK = new KareliumOreBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
+	private static ConfiguredFeature<?, ?> KAREL_ORE_OVERWORLD = Feature.ORE
+			.configure(new OreFeatureConfig(
+					OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
+					KAREL_ORE_BLOCK.getDefaultState(),
+					9)) // vein size
+			.decorate(Decorator.RANGE.configure(new RangeDecoratorConfig(
+					0,
+					12,
+					70)))
+			.spreadHorizontally()
+			.repeat(30); // number of veins per chunk
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -22,7 +43,10 @@ public class FinalProgrammingProjectMod implements ModInitializer {
 		Registry.register(Registry.ITEM, new Identifier("programmingproject", "karelium_ingot"), KAREL_INGOT_ITEM);
 		Registry.register(Registry.BLOCK, new Identifier("programmingproject", "karelium_ore"), KAREL_ORE_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier("programmingproject", "karelium_ore"), new BlockItem(KAREL_ORE_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
-
-		System.out.println("Hello from a programming student!");
+		RegistryKey<ConfiguredFeature<?, ?>> oreWoolOverworld = RegistryKey.of(Registry.CONFIGURED_FEATURE_WORLDGEN,
+				new Identifier("programmingproject", "ore_wool_overworld"));
+		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreWoolOverworld.getValue(), KAREL_ORE_OVERWORLD);
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreWoolOverworld);
+		System.out.println("Hello hello!");
 	}
 }
--- src/main/java/net/asolidtime/programmingproject/items/KareliumOreBlock.java
+++ src/main/java/net/asolidtime/programmingproject/items/KareliumOreBlock.java
@@ -1,4 +1,25 @@
 package net.asolidtime.programmingproject.items;
 
-public class KareliumOreBlock {
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class KareliumOreBlock extends Block {
+    public KareliumOreBlock(Settings settings) {
+        super(settings);
+    }
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            player.sendMessage(new LiteralText("Hello, world!"), false);
+        }
+
+        return ActionResult.SUCCESS;
+    }
 }
