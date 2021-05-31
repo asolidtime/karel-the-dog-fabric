c071e053a14a40e1658be2e76b576cb46472529c
Maxwell Leon <maxwellleon55243@gmail.com>
2021-05-30 19:55:25 -0500

    Getting Karelium Ore to smelt into Karelium Ingots

--- src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
+++ src/main/java/net/asolidtime/programmingproject/FinalProgrammingProjectMod.java
@@ -1,5 +1,6 @@
 package net.asolidtime.programmingproject;
 
+import net.asolidtime.programmingproject.items.KarelEggItem;
 import net.asolidtime.programmingproject.items.KareliumIngotItem;
 import net.asolidtime.programmingproject.items.KareliumOreBlock;
 import net.fabricmc.api.ModInitializer;
@@ -23,8 +24,9 @@ import net.minecraft.world.gen.feature.OreFeatureConfig;
 
 public class FinalProgrammingProjectMod implements ModInitializer {
 	public static final KareliumIngotItem KAREL_INGOT_ITEM = new KareliumIngotItem(new FabricItemSettings().group(ItemGroup.MISC));
+	public static final KarelEggItem KAREL_EGG_ITEM = new KarelEggItem(new FabricItemSettings().group(ItemGroup.MISC));
 	public static final KareliumOreBlock KAREL_ORE_BLOCK = new KareliumOreBlock(FabricBlockSettings.of(Material.METAL).strength(4.0f));
-	private static ConfiguredFeature<?, ?> KAREL_ORE_OVERWORLD = Feature.ORE
+	private static final ConfiguredFeature<?, ?> KAREL_ORE_OVERWORLD = Feature.ORE
 			.configure(new OreFeatureConfig(
 					OreFeatureConfig.Rules.BASE_STONE_OVERWORLD,
 					KAREL_ORE_BLOCK.getDefaultState(),
@@ -40,6 +42,7 @@ public class FinalProgrammingProjectMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
+		Registry.register(Registry.ITEM, new Identifier("programmingproject", "karel_egg"), KAREL_EGG_ITEM);
 		Registry.register(Registry.ITEM, new Identifier("programmingproject", "karelium_ingot"), KAREL_INGOT_ITEM);
 		Registry.register(Registry.BLOCK, new Identifier("programmingproject", "karelium_ore"), KAREL_ORE_BLOCK);
 		Registry.register(Registry.ITEM, new Identifier("programmingproject", "karelium_ore"), new BlockItem(KAREL_ORE_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
@@ -47,6 +50,6 @@ public class FinalProgrammingProjectMod implements ModInitializer {
 				new Identifier("programmingproject", "ore_wool_overworld"));
 		Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, oreWoolOverworld.getValue(), KAREL_ORE_OVERWORLD);
 		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, oreWoolOverworld);
-		System.out.println("Hello hello!");
+		System.out.println("(Karel) The Karel mod has been initialized, your game now has Karels!");
 	}
 }
---
+++ src/main/java/net/asolidtime/programmingproject/items/KarelEggItem.java
@@ -0,0 +1,10 @@
+package net.asolidtime.programmingproject.items;
+
+import net.minecraft.item.Item;
+
+
+public class KarelEggItem extends Item {
+    public KarelEggItem(Settings settings) {
+        super(settings);
+    }
+}
