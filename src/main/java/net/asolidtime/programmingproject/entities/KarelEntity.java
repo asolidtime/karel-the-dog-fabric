b02fe15a3edac84ce67719c319ebbd79a9879d42
Maxwell Leon <maxwellleon55243@gmail.com>
2021-06-07 10:40:22 -0500

    Adding goals to Karel's AI

--- src/main/java/net/asolidtime/programmingproject/entities/KarelEntity.java
+++ src/main/java/net/asolidtime/programmingproject/entities/KarelEntity.java
@@ -1,9 +1,20 @@
 package net.asolidtime.programmingproject.entities;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.passive.AnimalEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.Durations;
+import net.minecraft.entity.ai.goal.*;
+import net.minecraft.entity.mob.Angerable;
 import net.minecraft.entity.passive.PassiveEntity;
+import net.minecraft.entity.passive.TameableEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.IntRange;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -14,10 +25,18 @@ import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
 import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 
-public class KarelEntity extends AnimalEntity implements IAnimatable {
-    private AnimationFactory factory = new AnimationFactory(this);
+import java.util.UUID;
 
-    public KarelEntity(EntityType<? extends AnimalEntity> entityType, World world) {
+public class KarelEntity extends TameableEntity implements Angerable, IAnimatable {
+
+
+    private AnimationFactory factory = new AnimationFactory(this);
+    private UUID targetUuid;
+    private int angerTime;
+    private static final IntRange ANGER_TIME_RANGE = Durations.betweenSeconds(15, 20);
+    private TameableEntity tameable;
+
+    public KarelEntity(EntityType<? extends TameableEntity> entityType, World world) {
         super(entityType, world);
         this.ignoreCameraFrustum = true;
     }
@@ -30,7 +49,7 @@ public class KarelEntity extends AnimalEntity implements IAnimatable {
     @Nullable
     @Override
     public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
-        return null;
+        return EntityType.WOLF.create(world);
     }
 
     @Override
@@ -41,5 +60,90 @@ public class KarelEntity extends AnimalEntity implements IAnimatable {
     @Override
     public AnimationFactory getFactory() {
         return this.factory;
+    }
+
+    @Override
+    public int getAngerTime() {
+        return this.angerTime;
+    }
+
+    @Override
+    public void setAngerTime(int ticks) {
+        this.angerTime = ticks;
+    }
+
+    @Nullable
+    @Override
+    public UUID getAngryAt() {
+        return this.targetUuid;
+    }
+
+    @Override
+    public void setAngryAt(@Nullable UUID uuid) {
+        this.targetUuid = uuid;
+    }
+
+    @Override
+    public void chooseRandomAngerTime() {
+        this.setAngerTime(ANGER_TIME_RANGE.choose(this.random));
+    }
+
+    @Override
+    protected void initGoals() {
+        super.initGoals();
+        this.goalSelector.add(7, new LookAroundGoal(this));
+        this.goalSelector.add(5, new WanderAroundGoal(this, 2.0F));
+        this.goalSelector.add(6, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.add(5, new FollowOwnerGoal(this, 1.0D, 10.0F, 2.0F, false));
+        this.goalSelector.add(8, new WanderAroundFarGoal(this, 1.0D));
+        this.goalSelector.add(2, new SitGoal(this));
+        this.targetSelector.add(1, new TrackOwnerAttackerGoal(this));
+        this.targetSelector.add(2, new AttackWithOwnerGoal(this));
+        this.targetSelector.add(8, new UniversalAngerGoal(this, true));
+
+    }
+    @Override
+    public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        Item item = itemStack.getItem();
+        if (this.world.isClient) {
+            boolean bl = this.isOwner(player) || this.isTamed() || item == Items.EMERALD && !this.isTamed() && !this.hasAngerTime();
+            return bl ? ActionResult.CONSUME : ActionResult.PASS;
+        } else {
+            if (this.isTamed()) {
+                if (this.getHealth() < this.getMaxHealth()) {
+                    if (!player.abilities.creativeMode) {
+                        itemStack.decrement(1);
+                    }
+                    if (item == Items.EMERALD) {
+                        this.heal(4.0F);
+                    }
+                    return ActionResult.SUCCESS;
+                }
+                ActionResult actionResult = super.interactMob(player, hand);
+                if (!actionResult.isAccepted() && this.isOwner(player)) {
+                    this.setSitting(!this.isSitting());
+                    this.jumping = false;
+                    this.navigation.stop();
+                    this.setTarget((LivingEntity)null);
+                    return ActionResult.SUCCESS;
+                }
+                return actionResult;
+            } else if (item == Items.EMERALD && !this.hasAngerTime()) {
+                if (!player.abilities.creativeMode) {
+                    itemStack.decrement(1);
+                }
+
+
+                this.setOwner(player);
+                this.navigation.stop();
+                this.setTarget((LivingEntity)null);
+                this.setSitting(true);
+
+                return ActionResult.SUCCESS;
+            }
+
+            return super.interactMob(player, hand);
+        }
     }
 }
